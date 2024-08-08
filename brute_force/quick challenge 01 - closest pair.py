ea25c2a70c7083c750f77d156cd9de9d19cfa611
David Balleza Ayala <davidballezaa@gmail.com>
2024-08-08 15:36:17 -0600

    add 01 challenges

---
+++ brute_force/quick challenge 01 - closest pair.py
@@ -0,0 +1,17 @@
+# 1. Determina cuales son los dos puntos mas cercanos dadas sus coordenadas x, y
+
+
+import math
+
+coor = [[-2.423, -8.469],
+[5.721,	9.354],
+[6.766,	-3.823],
+[4.129,	6.744],
+[5.371,	-5.404]]
+
+min_distance = float("inf")
+
+for i in range(len(coor)):
+    for j in range(i + 1, len(coor)):
+        distance = 
+        min_distance = min(min_distance, )
