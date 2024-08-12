8ad54e742226ef02beb58df835c522f852d9a3dd
David Balleza Ayala <davidballezaa@gmail.com>
2024-08-12 18:49:28 +0000

    add solutions

--- brute_force/quick challenge 01 - closest pair.py
+++ brute_force/quick challenge 01 - closest pair.py
@@ -1,5 +1,7 @@
 # 1. Determina cuales son los dos puntos mas cercanos dadas sus coordenadas x, y
 
+# Time Complexity: O(N^2)
+# Space Complexity: O(1)
 
 import math
 
@@ -10,8 +12,17 @@ coor = [[-2.423, -8.469],
 [5.371,	-5.404]]
 
 min_distance = float("inf")
+min_points = []
 
 for i in range(len(coor)):
+    x1, y1 = coor[i]
     for j in range(i + 1, len(coor)):
-        distance = 
-        min_distance = min(min_distance, )
+        x2, y2 = coor[j]
+        # Distancia (tuve que googlear la formula lol)
+        distance = math.sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2))
+        # Updateamos distancia mínima y puntos más cercanos cada que sea menor
+        if distance < min_distance:
+            min_points = [coor[i], coor[j]]
+            min_distance = distance
+
+print("Puntos más cercanos entre sí:", min_points)
--- brute_force/quick challenge 01 - string matching.py
+++ brute_force/quick challenge 01 - string matching.py
@@ -1,9 +1,11 @@
-
-
 # 1. Determina cuales son los indices donde aparece patron en cadena 
 #    usando comparaciones entre caracteres unitarios. 
 #    La salida correcta deberia ser 10 y 15. Tu codigo debe averiguarlo. 
-# 2. Que complejidad tiene el algoritmo? 
+# 2. Que complejidad tiene el algoritmo.
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+
 
 cadena = "this is a small example"
 patron = "small"
@@ -11,6 +13,29 @@ patron = "small"
 print("Buscando: ", patron)
 print("Dentro de: ", cadena)
 
-print( patron[0] == cadena[0] ) # <--- ejemplo de comparacion entre caracteres
+def indicesPatron(cadena, patron):
+  # edge case:
+  if len(patron) > len(cadena):
+    return False
+  curr_patron_index = 0
+  start_index = -1
+  # iteramos por toda la cadena
+  for i in range(len(cadena)):
+    if cadena[i] == patron[curr_patron_index]:
+      # checamos si ya iniciamos el indice
+      if start_index == -1:
+        start_index = i
+      # si matchea y hemos acabado de recorrer el patron, retornamos True
+      if curr_patron_index == len(patron) - 1:
+        return [start_index, i + 1]
+      curr_patron_index += 1
+    else:
+      # cuando no hace match, reiniciamos apuntador del patron 
+      start_index = -1
+      curr_patron_index = 0
+  return False
+
+print(indicesPatron(cadena, patron))
+
 
 
