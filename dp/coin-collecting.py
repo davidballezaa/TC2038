5fd2484b2df2be98b1f91c8aa701170cea9636ee
David Balleza Ayala <davidballezaa@gmail.com>
2024-09-01 08:04:27 +0000

    add task ans

--- dp/coin-collecting.py
+++ dp/coin-collecting.py
@@ -5,6 +5,7 @@ def coinCollecting(matrix, n):
   coinsCollected = [[0 for i in range(n + 1)] for j in range(n + 1)] 
   for i in range(1, len(coinsCollected)):
     for j in range(1, len(coinsCollected[i])):
+      # Relación de recursion
       coinsCollected[i][j] = max(coinsCollected[i-1][j], coinsCollected[i][j-1]) + matrix[i-1][j-1]
 
   return coinsCollected[n][n]
@@ -17,3 +18,11 @@ grid = [
     [1, 0, 0, 0, 1],
 ]
 print(coinCollecting(grid, 5))
+
+"""
+¿Por qué funciona la relación de recurrencia? 
+
+La idea principal es que para llegar a una celda (i, j) en la matriz, solo puedes haber llegado desde la celda de arriba (i-1, j) o desde la celda de la izquierda (i, j-1).
+Por lo tanto, para calcular la cantidad máxima de monedas recolectadas en (i, j), necesitas conocer la cantidad máxima de monedas recolectadas en esas dos celdas previas.
+"""
+
